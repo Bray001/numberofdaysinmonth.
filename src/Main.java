d95466659d17fefc0473976ede6b6363d64e1dcb
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-09-16 12:45:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+public class Main {
+    public static void main(String[] args) {
+        printNumberInWord(0);
+        printNumberInWord(1);
+        printNumberInWord(2);
+        printNumberInWord(3);
+        printNumberInWord(4);
+        printNumberInWord(5);
+        printNumberInWord(6);
+        printNumberInWord(7);
+        printNumberInWord(8);
+        printNumberInWord(9);
+        printNumberInWord(10);
+        printNumberInWord(-1);
+    }
+
+    public static void printNumberInWord(int number) {
+
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+
+        public static void printNumberInWord(int number) {
+
+            switch (number) {
+                case 1:
+                    System.out.println("ONE");
+                    break;
+
+                default:
+                    System.out.println("OTHER");
+                    break;
+            }
+
+            public static void printNumberInWord(int number) {
+
+                switch (number) {
+                    case 0:
+                        System.out.println("ZERO");
+                        break;
+
+                    default:
+                        System.out.println("OTHER");
+                        break;
+                }
+
+                public static void printNumberInWord(int number) {
+
+                    switch (number) {
+                        case 0:
+                            System.out.println("ZERO");
+                            break;
+
+                        default:
+                            System.out.println("OTHER");
+                            break;
+                    }
+
+                }
+}
